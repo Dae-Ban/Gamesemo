07e340995664d4729dfca249c8beb78459af1f2c
Jaewon <wjwl0412@gmail.com>
2025-06-10 14:08:10 +0900

    Mapper 파일 수정

--- src/main/java/com/example/demo/mapper/GameInfoMapper.java
+++ src/main/java/com/example/demo/mapper/GameInfoMapper.java
@@ -2,8 +2,11 @@ package com.example.demo.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.example.demo.model.GameInfo;
 
-public class GameInfoMapper {
+@Mapper
+public interface GameInfoMapper {
 	List<GameInfo> selectTop10GamesByPlatform(String platform);
 }
--- src/main/java/com/example/demo/mapper/MemberMapper.java
+++ src/main/java/com/example/demo/mapper/MemberMapper.java
@@ -2,9 +2,11 @@ package com.example.demo.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.example.demo.model.Member;
 
-public class MemberMapper {
+@Mapper
+public interface MemberMapper {
     List<Member> selectAllEmailSubscribers();
-
 }
