4f57e0a3249f2c2a8efbf734d7812e738b94c917
Jaewon <wjwl0412@gmail.com>
2025-06-10 12:19:14 +0900

    sale email 기능 추가

--- src/main/java/com/example/demo/mapper/GameInfoMapper.java
+++ src/main/java/com/example/demo/mapper/GameInfoMapper.java
@@ -1,5 +1,10 @@
 package com.example.demo.mapper;
 
+import java.util.List;
+
+import com.example.demo.model.GameInfo;
+
 public class GameInfoMapper {
+    List<GameInfo> selectTop10GamesByPlatform(String platform);
 
 }
--- src/main/java/com/example/demo/service/EmailService.java
+++ src/main/java/com/example/demo/service/EmailService.java
@@ -4,5 +4,42 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+	package com.example.demo.service;
+
+	import java.io.File;
+
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.mail.javamail.JavaMailSender;
+	import org.springframework.mail.javamail.MimeMessageHelper;
+	import org.springframework.stereotype.Service;
+
+	import jakarta.mail.internet.MimeMessage;
+
+	@Service
+	public class EmailService {
+
+	    @Autowired
+	    private JavaMailSender mailSender;
+
+	    public void sendHtmlMessage(String to, String htmlContent) {
+	        try {
+	            MimeMessage message = mailSender.createMimeMessage();
+	            MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
+	            helper.setTo(to);
+	            helper.setSubject("오늘의 게임 할인 소식🎮");
+	            helper.setText(htmlContent, true);
+	            helper.setFrom("2j1william@gmail.com");
+	            helper.addInline("logoImage", new File("src/main/resources/static/images/logo.png"));
+	            helper.addInline("emailIcon", new File("src/main/resources/static/images/email.png"));
+	            helper.addInline("kakaoIcon", new File("src/main/resources/static/images/kakao.png"));
+	            
+	            mailSender.send(message);
+	            System.out.println("이메일 전송 완료: " + to);
+	        } catch (Exception e) {
+	            System.out.println("이메일 전송 실패: " + to);
+	            e.printStackTrace();
+	        }
+	    }
+	}
 
 }
