3ef01a84f689e939e64544402a2a141514eea65c
Jaewon <wjwl0412@gmail.com>
2025-06-10 12:25:37 +0900

    Revert "sale email 기능 추가"
    This reverts commit 4f57e0a3249f2c2a8efbf734d7812e738b94c917.

--- src/main/java/com/example/demo/mapper/GameInfoMapper.java
+++ src/main/java/com/example/demo/mapper/GameInfoMapper.java
@@ -1,10 +1,5 @@
 package com.example.demo.mapper;
 
-import java.util.List;
-
-import com.example.demo.model.GameInfo;
-
 public class GameInfoMapper {
-    List<GameInfo> selectTop10GamesByPlatform(String platform);
 
 }
--- src/main/java/com/example/demo/mapper/MemberMapper.java
+++ src/main/java/com/example/demo/mapper/MemberMapper.java
@@ -1,10 +1,5 @@
 package com.example.demo.mapper;
 
-import java.util.List;
-
-import com.example.demo.model.Member;
-
 public class MemberMapper {
-    List<Member> selectAllEmailSubscribers();
 
 }
