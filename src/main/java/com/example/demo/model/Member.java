b0fae733657eb150bb4f2da90141e3ed61cfb787
Jaewon <wjwl0412@gmail.com>
2025-06-11 14:58:55 +0900

    member DTO 에 이메일 인증 토큰 컬럼 추가됨.

--- src/main/java/com/example/demo/model/Member.java
+++ src/main/java/com/example/demo/model/Member.java
@@ -21,4 +21,5 @@ public class Member {
     private int state;                // 상태값 (활성/비활성 등)
     private String emailAd;           // 이메일 광고 수신 동의 여부
     private String emailVerified;     // 이메일 인증 여부
+    private String token;             // 이메일 인증 비교 토큰
 }
