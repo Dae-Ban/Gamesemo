6b7fc7a722e7c8e2b2b62d17256070f92d12b4f1
seongchan <ysc1086@gmail.com>
2025-06-20 14:05:55 +0900

    OAuth2 설정 (명시적 방식)

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @Configuration
 @EnableWebSecurity
@@ -23,24 +27,39 @@ public class SecurityConfig {
             .cors(cors -> cors.disable())
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/member/update").permitAll()
-                .anyRequest().permitAll()
+                .requestMatchers("/member/update", "/member/login").permitAll()
+                .anyRequest().authenticated()
             )
             .formLogin(form -> form.disable())
-
-
             .logout(logout -> logout
                 .logoutUrl("/member/logout")
                 .logoutSuccessUrl("/member/login")
                 .permitAll()
-            )
-
-
-            .oauth2Login(oauth2 -> oauth2
-                .loginPage("/member/login")
-                .defaultSuccessUrl("/oauth2/success")
             );
+
+        // OAuth2 설정 (명시적 방식)
+//        http.apply(new OAuth2LoginConfigurer<HttpSecurity>()
+//            .loginPage("/member/login") // 로그인 페이지 직접 지정
+//            .defaultSuccessUrl("/oauth2/success", true)
+//        );
 
         return http.build();
     }
+
+    
+    @Bean
+	 public HttpFirewall allowDoubleSlashFirewall() {
+	        StrictHttpFirewall firewall = new StrictHttpFirewall();
+	        // double slash 허용
+	        firewall.setAllowUrlEncodedDoubleSlash(true); // URL 인코딩된 // 허용 (%2F%2F)
+//	        firewall.setAllowBackSlash(true); // (필요한 경우만)
+//	        firewall.setAllowSemicolon(true); // (필요한 경우만)
+//	        firewall.setAllowUrlEncodedSlash(true); // %2F 허용 (선택)
+	        return firewall;
+	    }
+	 
+	 @Bean
+	    public WebSecurityCustomizer webSecurityCustomizer(HttpFirewall firewall) {
+	        return web -> web.httpFirewall(firewall);
+	    }
 }
