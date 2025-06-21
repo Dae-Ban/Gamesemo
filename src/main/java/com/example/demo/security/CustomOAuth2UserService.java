ff65ea4e7c8ecd961a09a9115d3563b0fd20e792
user <user@DESKTOP-01SQNGC>
2025-06-21 16:40:30 +0900

    재원님과 합치기 전, 영교님(OAut서비스수정)

---
+++ src/main/java/com/example/demo/security/CustomOAuth2UserService.java
@@ -0,0 +1,110 @@
+package com.example.demo.security;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.mapper.MemberMapper;
+import com.example.demo.model.Member;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+	private final MemberMapper memberMapper;
+	private final HttpSession session; // 세션객체 추가함(지선)
+
+
+	@Override
+	public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException {
+		OAuth2User oAuth2User = super.loadUser(request);
+
+		String platform = request.getClientRegistration().getRegistrationId();
+		Map<String, Object> attributes = oAuth2User.getAttributes();
+		
+		String socialId = null;
+		String name = null;
+		String email = null;
+
+		if ("google".equals(platform)) {
+			socialId = attributes.get("sub").toString();
+			name = (String) attributes.get("name");
+			email = (String) attributes.get("email");
+		} else if ("naver".equals(platform)) {
+			attributes = (Map<String, Object>) attributes.get("response");
+			socialId = attributes.get("id").toString();
+			name = (String) attributes.get("name");
+			email = (String) attributes.get("email");
+		} else if ("kakao".equals(platform)) {
+			socialId = attributes.get("id").toString();
+			Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+		    Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+		    name = (String) profile.get("nickname");
+		    email = (String) kakaoAccount.get("email");
+		} else {
+			throw new IllegalArgumentException("Unknown platform: " + platform);
+		}
+		
+		System.out.println("name:" + name);
+		System.out.println("email:" + email);
+		
+		String emailId = "", emailDomain = "";
+		if (email != null && email.contains("@")) {
+			String[] emailParts = email.split("@");
+			emailId = emailParts[0];
+			emailDomain = emailParts[1];
+		}
+
+		Member member = findBySocialIdAndPlatform(socialId, platform);
+		if (member == null) {
+			member = new Member();
+			String shortUUID = UUID.randomUUID().toString().substring(0, 30);
+			member.setId(shortUUID);
+			member.setName(name);
+//			member.setNickname(name);
+			member.setSocialPlatform(platform);
+			member.setSocialId(socialId);
+			member.setEmailId(emailId);
+			member.setEmailDomain(emailDomain);
+			member.setPw("SOCIAL");
+			member.setBirthDate("1900-01-01");
+			member.setPhone("000-0000-0000");
+			member.setGender("N");
+			member.setEmailAd("N");
+			member.setState(0);
+			member.setEmailVerified("Y");
+			memberMapper.socialInsert(member);
+			
+			//추가(지선)
+			member = memberMapper.findBySocialIdAndPlatform(socialId, platform); 
+		}
+
+		// 마이페이지에 소셜로그인 정보 가져오기 추가 (지선)
+		session.setAttribute("loginMember", member);
+		
+		List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+		
+		if(platform.equals("kakao"))
+			return new DefaultOAuth2User(authorities, attributes, "id");
+		else
+			return new DefaultOAuth2User(authorities, attributes, "name");
+	}
+
+	public Member findBySocialIdAndPlatform(String socialId, String platform) {
+		return memberMapper.findBySocialIdAndPlatform(socialId, platform);
+	}
+}
