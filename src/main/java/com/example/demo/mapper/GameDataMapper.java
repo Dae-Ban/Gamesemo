c7abbaa706d2c0d965b4f7ca0668e02e631a0d92
seongchan <ysc1086@gmail.com>
2025-06-23 09:40:57 +0900

    오타 수정

--- src/main/java/com/example/demo/mapper/GameDataMapper.java
+++ src/main/java/com/example/demo/mapper/GameDataMapper.java
@@ -7,17 +7,17 @@ import com.example.demo.model.Game;
 public interface GameDataMapper {
 	// 스팀 api
 	public void insertSteamApi(List<Game> batch);
-	public void margeSteamApi(Game steamApi);
+	public void mergeSteamApi(Game steamApi);
 	public List<Long> selectAllAppIds();
 	public int getGNum(String nTitle);
 	
 	// from 스크랩
-	public void margeSteamTop();
-	public void margeNintendoExp();
-	public void margeSteamDC();
-	public void margeSteamNew();
-	public void margeDirectNew();
-	public void margeNintendoDC();
-	public void margeNintendoNew();
-	public void margePlanetNew();
+	public void mergeSteamTop();
+	public void mergeNintendoExp();
+	public void mergeSteamDC();
+	public void mergeSteamNew();
+	public void mergeDirectNew();
+	public void mergeNintendoDC();
+	public void mergeNintendoNew();
+	public void mergePlanetNew();
 }
