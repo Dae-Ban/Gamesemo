a02a7d416ea8ad2fa48a3cd1f9e4e28f7b970756
seongchan <ysc1086@gmail.com>
2025-06-25 15:32:12 +0900

    배포 방식을 war로 변경

--- src/main/java/com/example/demo/GamesemoApplication.java
+++ src/main/java/com/example/demo/GamesemoApplication.java
@@ -3,15 +3,21 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @MapperScan("com.example.demo.mapper")
-public class GamesemoApplication {
+public class GamesemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamesemoApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GamesemoApplication.class);
+    }
 }
